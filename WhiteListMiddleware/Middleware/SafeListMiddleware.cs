6dca5b4184abf7fe2b974616c377d599e8e283e3
Danilo de Oliveira <doliveira.doti@bancomaxima.com.br>
2018-09-06 09:15:58 -0300

    Adicionando o projeto

---
+++ WhiteListMiddleware/Middleware/SafeListMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace WhiteListMiddleware.Middleware
+{
+    public class SafeListMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<SafeListMiddleware> _logger;
+        private readonly string _adminSafeList;
+
+        public SafeListMiddleware(RequestDelegate next, ILogger<SafeListMiddleware> logger, string adminWhiteList)
+        {
+            _adminSafeList = adminWhiteList;
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == "GET" || context.Request.Method == "POST")
+            {
+                var remoteIp = context.Connection.RemoteIpAddress;
+                _logger.LogDebug($"Request from Remote IP address: {remoteIp}");
+
+                string[] ip = _adminSafeList.Split(';');
+
+                var bytes = remoteIp.GetAddressBytes();
+                var badIp = true;
+                foreach (var address in ip)
+                {
+                    var testIp = IPAddress.Parse(address);
+                    if (testIp.GetAddressBytes().SequenceEqual(bytes))
+                    {
+                        badIp = false;
+                        break;
+                    }
+                }
+
+                if (badIp)
+                {
+                    _logger.LogInformation($"Forbidden Request from Remote IP address: {remoteIp}");
+                    context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                    return;
+                }
+            }
+
+            await _next.Invoke(context);
+
+        }
+    }
+}
---
+++ WhiteListMiddleware/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using WhiteListMiddleware.Middleware;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WhiteListMiddleware
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseMiddleware<SafeListMiddleware>(Configuration["SafeList"]);
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
